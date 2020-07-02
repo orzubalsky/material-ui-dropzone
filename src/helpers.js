6dfab16a742c1bbfe9f139e19e58867bddb8bea1
orzubalsky <orzubalsky@gmail.com>
2020-07-02 09:30:54 -0400

    Replce readFile helper with a function that parses files chunk by chunk

--- src/helpers.js
+++ src/helpers.js
@@ -25,6 +25,7 @@ export async function createFileFromUrl(url) {
     return new File([data], filename, metadata);
 }
 
+/*
 export function readFile(file) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
@@ -36,5 +37,47 @@ export function readFile(file) {
             reject(event);
         };
         reader.readAsDataURL(file);
+    });
+}
+*/
+
+export function parseFile (file, callback) {
+    const fileSize   = file.size;
+    const chunkSize  = 64 * 1024; // bytes
+    const offset     = 0;
+    const self       = this; // we need a reference to the current object
+    let chunkReaderBlock = null;
+
+    const readEventHandler = evt => {
+        if (evt.target.error == null) {
+            offset += evt.target.result.length;
+            callback(evt.target.result); // callback for handling read chunk
+        } else {
+            console.log("Read error: " + evt.target.error);
+            return;
+        }
+        if (offset >= fileSize) {
+            console.log("Done reading file");
+            return;
+        }
+
+        // of to the next chunk
+        chunkReaderBlock(offset, chunkSize, file);
+    }
+
+    chunkReaderBlock = (_offset, length, _file) => {
+        const r = new FileReader();
+        const blob = _file.slice(_offset, length + _offset);
+        r.onload = readEventHandler;
+        r.readAsText(blob);
+    }
+
+    // now let's start the read with the first block
+    chunkReaderBlock(offset, chunkSize, file);
+}
+
+export function readFile(file) {
+    return new Promise((resolve, reject) => {
+        parseFile(file, result => resolve(result))
     });
 }
